8f31c0f77f0bf7e72844f94a5ffd7d56da7afd76
Kannav <kmkannavkmehta@gmail.com>
2022-10-02 14:53:47 +0530

    add bazel install instructions

---
+++ misc/install_bazel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /etc/apt/keyrings
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+
