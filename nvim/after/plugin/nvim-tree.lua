987789c39a44b164238c23dbe71f7689e643ecc2
Kannav <kmkannavkmehta@gmail.com>
2023-04-04 14:49:32 +0530

    switch vim to lua

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,24 +1,24 @@
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
+-- keymaps
+vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>r", ":NvimTreeRefresh<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>n", ":NvimTreeFindFile<CR>", {noremap = true, silent = true})
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
+    sort_by = "case_sensitive",
+    view = {
+        width = 30,
+        mappings = {
+            list = {{
+                key = "u",
+                action = "dir_up"
+            }}
+        }
     },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    renderer = {
+        group_empty = true
+    },
+    filters = {
+        dotfiles = true
+    }
 })
+
